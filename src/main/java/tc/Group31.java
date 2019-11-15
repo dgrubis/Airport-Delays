ec9dd699788ce52293e2f4601d7d902e2f766823
harshit21g <gupta.hars@husky.neu.edu>
2019-11-15 10:30:05 -0500

    code structure

--- src/main/java/tc/Group31.java
+++ src/main/java/tc/Group31.java
@@ -22,52 +22,52 @@ public class Group31 extends Configured implements Tool {
 	private static final Logger logger = LogManager.getLogger(Group31.class);
 
 	public static class Group31Mapper extends Mapper<Object, Text, Text, IntWritable> {
-		private final static IntWritable one = new IntWritable(1);
-		private final Text follower = new Text();
 
 		@Override
 		public void map(final Object key, final Text value, final Context context) throws IOException, InterruptedException {
-			String[] id = value.toString().split(",");
-			String followerid = id[1];
-			follower.set(followerid);
 			context.write(follower, one);
 		}
 	}
 
 	public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-		private final IntWritable result = new IntWritable();
-
+		
 		@Override
 		public void reduce(final Text key, final Iterable<IntWritable> values, final Context context) throws IOException, InterruptedException {
-			int sum = 0;
-			for (final IntWritable val : values) {
-				sum += val.get();
-			}
-			result.set(sum);
 			context.write(key, result);
 		}
 	}
 
 	@Override
 	public int run(final String[] args) throws Exception {
+
+		//COnfiguration
 		final Configuration conf = getConf();
 		final Job job = Job.getInstance(conf, "Follower Count");
 		job.setJarByClass(Group31.class);
 		final Configuration jobConf = job.getConfiguration();
 		jobConf.set("mapreduce.output.textoutputformat.separator", "\t");
+
+
 		// Delete output directory, only to ease local development; will not work on AWS. ===========
 //		final FileSystem fileSystem = FileSystem.get(conf);
 //		if (fileSystem.exists(new Path(args[1]))) {
 //			fileSystem.delete(new Path(args[1]), true);
 //		}
 		// ================
+
+		//Classes for mapper, combiner and reducer
 		job.setMapperClass(Group31Mapper.class);
 		job.setCombinerClass(IntSumReducer.class);
 		job.setReducerClass(IntSumReducer.class);
+
+		//Key and Value type for output
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
+
+		//Path for input and output
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
 		return job.waitForCompletion(true) ? 0 : 1;
 	}
 
