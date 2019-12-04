c82f94ded5f158710a97a3321e024ef8cee6d796
dgrubis <dgrubis@10.0.1.33>
2019-12-04 18:16:09 -0500

    add mllib to pom.xml

--- sparkClassification/SparkClassification/src/main/scala/project/SparkClassification/DecisionTreeModel.scala
+++ sparkClassification/SparkClassification/src/main/scala/project/SparkClassification/DecisionTreeModel.scala
@@ -5,6 +5,10 @@ import org.apache.spark.SparkContext
 import org.apache.log4j.LogManager
 import org.apache.log4j.Level
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier} 
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
 
 object DTmodelMain {
   
@@ -23,6 +27,9 @@ object DTmodelMain {
     
     val df = sparkSession.read.option("inferSchema","true").csv(args(0)).toDF() //read in the joined data as a dataframe                 
     
-    
+    val LabelIndexer = new StringIndexer()
+                           .setInputCol("WEATHER_DELAY")
+                           .setOutputCol("label")
+                           .fit(df)
   }
 }
