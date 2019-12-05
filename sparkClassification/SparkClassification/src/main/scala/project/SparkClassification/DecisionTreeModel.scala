0a83f6098657c608e5772233134f891b03fe3b7b
dgrubis <dgrubis@10.0.1.33>
2019-12-05 01:21:18 -0500

    added features

--- sparkClassification/SparkClassification/src/main/scala/project/SparkClassification/DecisionTreeModel.scala
+++ sparkClassification/SparkClassification/src/main/scala/project/SparkClassification/DecisionTreeModel.scala
@@ -6,7 +6,9 @@ import org.apache.log4j.LogManager
 import org.apache.log4j.Level
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier} 
+import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
+import org.apache.spark.ml.classification.{DecisionTreeClassificationModel, DecisionTreeClassifier}
+import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
 
@@ -25,14 +27,31 @@ object DTmodelMain {
                        .config(conf)
                        .getOrCreate() 
     
-    val df = sparkSession.read.option("inferSchema","true").csv(args(0)).toDF() //read in the joined data as a dataframe                 
+    val df = sparkSession.read.option("inferSchema","true").csv(args(0)).toDF() //read in the joined data as a dataframe
+                                                                                //may need to name columns manually
     
-    val LabelIndexer = new StringIndexer()
-                           .setInputCol("WEATHER_DELAY")
-                           .setOutputCol("label")
-                           .fit(df) //sets the label column to be used in the algorithm as the weather delay flag (0 for no delay, 1 else)
-    
+    //val LabelIndexer = new StringIndexer()
+                           //.setInputCol("WEATHER_DELAY")
+                           //.setOutputCol("label")
+                           //.fit(df)
+    //WEATHER_DELAY is already a 0 or 1 flag
                            
-  
+    val myFeatures = Array("ORIGIN_LAT", "ORIGIN_LONG",
+                         "DEST_LAT", "DEST_LON", "AIRLINE",
+                         "TAIL_NUMBER",	"DISTANCE",
+                         //Origin
+                         "TEMP", "DEWP", "SLP", "STP", "VISIB",
+                         "WDSP", "MXSPD", "GUST",	"MAX", "MIN",	"PRCP",
+                         "SNDP", "FRSHTT",
+                         //Destination
+                         "TEMP", "DEWP", "SLP", "STP", "VISIB",
+                         "WDSP", "MXSPD", "GUST",	"MAX", "MIN",	"PRCP",
+                         "SNDP", "FRSHTT")
+     
+    val FeatureIndexer = new VectorAssembler()
+                             .setInputCols(myFeatures)
+                             .setOutputCol("features") //sets the feature vector as one column consisting of all specified features                   
+    
+                             
   }
 }
